e28068259b616dd7e592fc22a9e1b2e42ca76884
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-10 10:06:47 +0530

    Refactor: updated auth middlewear

--- middlewear/auth.middlewear.js
+++ middlewear/auth.middlewear.js
@@ -5,16 +5,14 @@ exports.auth = async (req, res, next) => {
     try{
         const token = req.headers.authorization?.split(" ")[1]
         if(!token){
-            return res.status(401).json({ message: "Unauthorized" })
+            return res.status(401).json({ message: "Token not found" })
         }
 
-        const response = axios.post(`${process.env.USER_SERVICE_URL}/user/verifyToken`, {}, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+        const response = await axios.post(`${process.env.USER_SERVICE_URL}/user/verifyToken`, {
+            token: token
         })
         if(response.status !== 200){
-            return res.status(401).json({ message: "Unauthorized" })
+            return res.status(401).json({ message: "Unauthorized access" })
         }
 
         req.user = response?.data?.user
@@ -22,6 +20,8 @@ exports.auth = async (req, res, next) => {
         next()
 
     }catch(err){
+
+
         console.error("Error in auth middleware:", err)
         return res.status(500).json({ message: "Internal server error" })
     }
