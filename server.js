a74b9aaf2d46b5dfbf57c5c5a88ea2caea3cd49d
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-10 09:43:03 +0530

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { conn } = require('./config/gridfsConnection.js');
 const { dbConnect } = require('./config/database.js');
+const fileRoute = require('./routes/file.route.js')
 
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors())
+
+app.use('/code', fileRoute)
 
 dbConnect()
 
