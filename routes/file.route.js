e5b9b3622ecaf1087b5274142957fc58bc3e1dd6
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-10 10:07:10 +0530

    Refactor: modified upload file route

--- routes/file.route.js
+++ routes/file.route.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { uploadFile, getFile, deleteFile} = require('../contorllers/file.controller')
-const { storage } = require('../config/gridfsConnection');
+const { uploadFile, getFile, deleteFile } = require('../contorllers/file.controller')
 const { auth } = require('../middlewear/auth.middlewear');
+require('dotenv').config()
+const GridFsStorage = require('multer-gridfs-storage');
 
-const upload = multer({ storage })
+const upload = multer({storage: multer.memoryStorage()})
 
-router.post('/upload', auth, upload.single("file"), uploadFile)
-router.get('/file/:id', getFile)
-router.delete('/file/:id', deleteFile)
+router.post('/upload', auth, upload.single('file'), uploadFile);
+router.get('/file/:id', auth, getFile)
+router.delete('/file/:id', auth, deleteFile)
 
 module.exports = router
